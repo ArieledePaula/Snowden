f1218af669cce237effd856ce2632ed19a3c6ee5
Ariele <arielecohendepaula@gmail.com>
2025-05-04 17:35:09 -0300

    Adicioando Texto Snowden e Estilizando

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ function aceitar() {
 
 
 function Guia() {
+
     const guia = document.getElementById("guia");
     guia.style.display = guia.style.display === "block" ? "none" : "block";
   }
+  
+  
